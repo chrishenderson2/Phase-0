cd9da91b35c02b6f335256ad4defda73f21be0c9
chrishenderson2 <chrishendo510@gmail.com>
2016-01-31 13:59:42 -0800

    good-guess, triangles, smallest int

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,21 @@
+# Good Guess
+
+# I worked on this challenge [by myself, with: ].
+
+#with che sanders
+
+# Your Solution Below
+
+puts "input integer"
+
+
+
+def good_guess?(int)
+	if int == 42
+		true
+	else
+		false
+	end
+end
+
+good_guess?(42)
---
+++ week-4/triangle-side/my_solution.rb
@@ -0,0 +1,31 @@
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+
+
+def valid_triangle?(a, b, c)
+  if (a == 0 || b == 0 || c == 0)
+    return false
+  elsif ((a + b <= c ) || (a + c <= b) || (b + c <= a))
+    return false
+  elsif ((a == b) && (a == c) && (b == c))
+    return true
+  elsif ((a == b) || (a == c) || (b == c))
+    return true
+  elsif ((a+2*b+2*c) && (2*a+b+2*c) && (2*a+2*b+3*c))
+    return true
+  else
+    return false
+  end
+end
+
+  class
+  	
+
+
+
+
+end
