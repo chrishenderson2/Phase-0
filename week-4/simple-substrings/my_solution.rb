ebb8a206a2e5140ae0ed4baa0ef3cb10e57c660c
chrishenderson2 <chrishendo510@gmail.com>
2016-02-01 01:08:08 -0800

    initial commit

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,16 @@
+# Simple Substrings
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+
+def welcome(address)
+    if address.include? ("CA")
+        "Welcome to California"
+    else
+        "You should move to California"
+    end
+end
+welcome("565 oak Street Oakland CA 94619")
